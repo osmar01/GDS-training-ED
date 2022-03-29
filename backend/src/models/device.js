d8c743ea06a269f01c9e3b73485f158602c74584
osmar01 <osmarj.frota@gmail.com>
2022-03-28 21:36:10 -0400

    commit initial - estrutura e modelagem

---
+++ backend/src/models/device.js
@@ -0,0 +1,12 @@
+
+class Device {
+
+  constructor(id, category, color, partNumber, ){
+    this.id - id,
+    this.category = category,
+    this.color = color,
+    this.partNumber = partNumber
+  }
+}
+
+module.exports = Device
